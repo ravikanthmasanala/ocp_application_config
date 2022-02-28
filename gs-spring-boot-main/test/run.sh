ae0a8b41dabddca8c331c5569a5b98e30390154b
capuser-factory <100305787+ravikanthmasanala@users.noreply.github.com>
2022-02-28 17:18:23 -0500

    Update run.sh

--- gs-spring-boot-main/test/run.sh
+++ gs-spring-boot-main/test/run.sh
@@ -3,14 +3,14 @@ cd $(dirname $0)
 id
 whoami
 cd ../complete
-home/capuser/maven/apache-maven-3.8.4/bin/mvn clean package
+/home/capuser/maven/apache-maven-3.8.4/bin/mvn clean package
 ret=$?
 if [ $ret -ne 0 ]; then
 exit $ret
 fi
 #rm -rf target
 
-home/capuser/maven/apache-maven-3.8.4/bin/mvn package 
+/home/capuser/maven/apache-maven-3.8.4/bin/mvn package 
 ret=$?
 if [ $ret -ne 0 ]; then
 exit $ret
