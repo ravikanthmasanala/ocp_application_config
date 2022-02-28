31684112adfd59b589cc3b0be994c670fe809731
capuser-factory <100305787+ravikanthmasanala@users.noreply.github.com>
2022-02-28 17:16:39 -0500

    Update build.sh

--- gs-spring-boot-main/build.sh
+++ gs-spring-boot-main/build.sh
@@ -3,6 +3,6 @@ cd initial/
 echo "cleaning mvn build"
  /home/capuser/maven/apache-maven-3.8.4/bin/mvn clean
 echo "Building mvn package"
-/home/capuser/maven/apache-maven-3.8.4/bin/mvn package
+#/home/capuser/maven/apache-maven-3.8.4/bin/mvn package
 
 
